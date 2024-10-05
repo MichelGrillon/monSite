623e7790f4c71d99add1fe2e54d0a1911633b6bc
Michel Grillon <mgrillon@gmail.com>
2024-10-05 19:51:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,9 @@ include 'pages/header.php';
         </div>
         <div class="right-header">
             <h1 class="name">
-                Bonjour, je suis <span><br>Michel Grillon.</span></h1><br>
+                Bonjour, 
+            </h1>
+            <p>je suis <span><br>Michel Grillon.</span></p><br>
             <h2>Développeur web junior.</h2><br>
             <hr><br>
             <h3>Site en construction et en tests.</h3>
